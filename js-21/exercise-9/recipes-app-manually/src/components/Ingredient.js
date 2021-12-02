f442b9bf0b74cf5bc87511860834ac028a395907
Markus Ijäs <markus.ijas@outlook.com>
2021-12-02 11:44:08 +0200

    Adds JS-21 E9 manual-recipes-app

---
+++ js-21/exercise-9/recipes-app-manually/src/components/Ingredient.js
@@ -0,0 +1,13 @@
+// .src/components/Ingredient.js
+
+import React from "react";
+
+const Ingredient = ({ amount, measurement, name }) => {
+  return (
+    <li>
+      {amount} {measurement} {name}
+    </li>
+  );
+}
+
+export default Ingredient;
---
+++ js-21/exercise-9/recipes-app-manually/src/components/IngredientsList.js
@@ -0,0 +1,16 @@
+// .src/components/IngredientsList.js
+
+import React from "react";
+import Ingredient from "./Ingredient";
+
+const IngredientsList = ({ list }) => {
+  return (
+    <ul className="ingredients">
+      {list.map((ingredient, i) => (
+        <Ingredient key={i} {...ingredient} />
+      ))}
+    </ul>
+  );
+}
+
+export default IngredientsList;
