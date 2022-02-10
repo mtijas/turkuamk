6c34fe15ed66fc3b8fd1a4b62a6058c1ff3f63be
Markus Ijäs <markus.ijas@outlook.com>
2022-02-10 11:07:51 +0200

    t3 evaluator now handles evaluationscale in any order

--- sswd-22/e02/t3/evaluator.js
+++ sswd-22/e02/t3/evaluator.js
@@ -14,7 +14,7 @@ exports.getGrade = function (points) {
   grade = 0;
 
   evaluationScale.forEach((element) => {
-    if (points >= element.points) {
+    if (points >= element.points && grade < element.grade) {
       grade = element.grade;
     }
   });
